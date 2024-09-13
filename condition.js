c9089b513565f92f120b794672217cb4ffe7df42
kousra <mdkousarmia71@gmail.com>
2024-09-13 16:29:24 +0600

    java script practis

--- condition.js
+++ condition.js
@@ -17,11 +17,36 @@ if(isRaning === true){
 // isRaning = false ? console.log("will not go outside"): console.log("will go outside");
 
 // multifull condition
-var money = 10;
+// var money = 10;
+// var isRaning = false;
+// var taxi = 15;
+// if(isRaning === false || money >= taxi) {
+//     console.log('will go to market'); 
+// }else {
+//     console.log('will not go to market');
+// }
+
+//multifull condition if else
+// var money = 40;
+// if ( money >= 100) {
+//     console.log( "biraney khabo");
+// }else if (money >= 50) {
+//     console.log('nodols khabo');   
+// }else {
+//     console.log('na kheya auto kore bari ferot jabo');
+    
+// }
 var isRaning = false;
-var taxi = 15;
-if(isRaning === false || money >= taxi) {
-    console.log('will go to market'); 
+var haveumralla = true;
+
+if (isRaning === true){
+    if(haveumralla =true){
+        console.log("will go outside, i have my chata");
+    }else {
+        console.log('will not go outside, cata nai');
+        
+    }
 }else {
-    console.log('will not go to market');
+    console.log('will go outside... its a sunny day');
+    
 }
